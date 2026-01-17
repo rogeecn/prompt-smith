bf470966406c0c0e94ce531f7e5d2546f1c11b60
Rogee <rogee@ipao.vip>
2026-01-17 17:37:39 +0800

    Fix account auth import

--- src/app/account/page.tsx
+++ src/app/account/page.tsx
@@ -1,6 +1,6 @@
 import { redirect } from "next/navigation";
 import AccountClient from "../../../components/AccountClient";
-import { getSession } from "../lib/auth";
+import { getSession } from "../../lib/auth";
 
 export default async function AccountPage() {
   const session = await getSession();
