7051a82000fbb6f12f5ed252501f87df939d12d3
Rogee <rogee@ipao.vip>
2026-01-18 17:05:47 +0800

    Make project grid single column on mobile

--- components/Dashboard.tsx
+++ components/Dashboard.tsx
@@ -172,7 +172,7 @@ export default function Dashboard() {
               暂无项目，请先创建一个新的项目。
             </div>
           ) : (
-            <div className="grid gap-4 sm:grid-cols-2">
+            <div className="grid gap-4 grid-cols-1 sm:grid-cols-2">
               {projectList.map((project) => (
                 <div
                   key={project.id}
